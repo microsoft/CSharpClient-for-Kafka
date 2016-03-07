04566da08bbb9c32422df2cb2a9fd91283509059
John Stark <imissjohn@gmail.com>
2016-03-07 16:34:16 -0700

    Fix for 0.9 producer
    Adding a space for DefaultClientId was required for kafkanet to work with Kafka 0.9 https://issues.apache.org/jira/browse/KAFKA-3088

--- src/KafkaNET.Library/Cfg/SyncProducerConfiguration.cs
+++ src/KafkaNET.Library/Cfg/SyncProducerConfiguration.cs
@@ -35,7 +35,7 @@ namespace Kafka.Client.Cfg
 
         public const int DefaultCorrelationId = -1;
 
-        public const string DefaultClientId = "";
+        public const string DefaultClientId = " ";
 
         public const short DefaultRequiredAcks = 0;
 
