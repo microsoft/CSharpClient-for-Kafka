935353eb2d1325c5d708f1c7312eadc8f11454af
Greg Fodor <gfodor@gmail.com>
2016-03-16 16:47:42 -0700

    Update tests for v1 FetchResponse

--- src/Kafka.Client.Tests/Response/FetchResponseTest.cs
+++ src/Kafka.Client.Tests/Response/FetchResponseTest.cs
@@ -19,32 +19,58 @@ namespace Kafka.Client.Tests.Response
     {
         [TestMethod]
         [TestCategory(TestCategories.BVT)]
-        public void ShouldAbleToParseFetchResponse()
+        public void ShouldAbleToParseV0FetchResponse()
         {
             var stream = new MemoryStream();
-            var writer = new KafkaBinaryWriter(stream);
-            writer.Write(1);
-            writer.Write(123); // correlation id
-            writer.Write(1); // data count
-            writer.WriteShortString("topic1");
-            writer.Write(1); // partition count
-            writer.Write(111); //partition id
-            writer.Write((short)ErrorMapping.NoError);
-
-            writer.Write(1011L); // hw            
-            var messageStream = new MemoryStream();
-            var messageWriter = new KafkaBinaryWriter(messageStream);
-            new BufferedMessageSet(new List<Message>() { new Message(new byte[100]) }, 0).WriteTo(messageWriter);
-            writer.Write((int)messageStream.Length);
-            writer.Write(messageStream.GetBuffer(), 0, (int)messageStream.Length);
-            stream.Seek(0, SeekOrigin.Begin);
+            WriteTestFetchResponse(stream, 0);
             var reader = new KafkaBinaryReader(stream);
             var response = new FetchResponse.Parser(0).ParseFrom(reader);
+            response.ThrottleTime.ShouldBeEquivalentTo(0);
             var set = response.MessageSet("topic1", 111);
             set.Should().NotBeNull();
             var messages = set.Messages.ToList();
             messages.Count().Should().Be(1);
             messages.First().Payload.Length.Should().Be(100);
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategories.BVT)]
+        public void ShouldAbleToParseV1FetchResponse()
+        {
+            var stream = new MemoryStream();
+            WriteTestFetchResponse(stream, 1);
+            var reader = new KafkaBinaryReader(stream);
+            var response = new FetchResponse.Parser(1).ParseFrom(reader);
+            response.ThrottleTime.ShouldBeEquivalentTo(456);
+            var set = response.MessageSet("topic1", 111);
+            set.Should().NotBeNull();
+            var messages = set.Messages.ToList();
+            messages.Count().Should().Be(1);
+            messages.First().Payload.Length.Should().Be(100);
+        }
+
+        private static void WriteTestFetchResponse(MemoryStream stream, int versionId)
+        {
+            var writer = new KafkaBinaryWriter(stream);
+            writer.Write(1);
+            writer.Write(123); // correlation id
+            if (versionId > 0)
+            {
+               writer.Write(456); 
+            }
+            writer.Write(1); // data count
+            writer.WriteShortString("topic1");
+            writer.Write(1); // partition count
+            writer.Write(111); //partition id
+            writer.Write((short) ErrorMapping.NoError);
+
+            writer.Write(1011L); // hw            
+            var messageStream = new MemoryStream();
+            var messageWriter = new KafkaBinaryWriter(messageStream);
+            new BufferedMessageSet(new List<Message>() {new Message(new byte[100])}, 0).WriteTo(messageWriter);
+            writer.Write((int) messageStream.Length);
+            writer.Write(messageStream.GetBuffer(), 0, (int) messageStream.Length);
+            stream.Seek(0, SeekOrigin.Begin);
         }
     }
 }
